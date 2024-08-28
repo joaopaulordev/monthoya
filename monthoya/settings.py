295ad54d9cfca4bcfabf273cc2fbb46b77bef8f0
João Paulo Rodrigues <joaopaulo.dev@hotmail.com>
2024-08-28 12:45:44 -0300

    first commit

--- monthoya/settings.py
+++ monthoya/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xynlxo*%nt15apr(%x23#1$z9ruaf$@c8)7i@ebu&h#2&l@*l4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # STATICFILES_DIRS = [

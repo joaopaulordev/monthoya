1f5b73d531cdc3e1faf33488fbf12c2e78b3c3c0
João Paulo Rodrigues <joaopaulo.dev@hotmail.com>
2024-08-30 20:19:28 -0300

    static files final

--- monthoya/settings.py
+++ monthoya/settings.py
@@ -149,7 +149,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static")    

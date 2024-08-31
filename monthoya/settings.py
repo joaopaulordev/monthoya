656c98ded666614eae98ea2d8086b05687c1f0ed
João Paulo Rodrigues <joaopaulo.dev@hotmail.com>
2024-08-31 13:26:06 -0300

    static files final 13

--- monthoya/settings.py
+++ monthoya/settings.py
@@ -154,7 +154,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # MEDIA_URL = "imagens/"
 
